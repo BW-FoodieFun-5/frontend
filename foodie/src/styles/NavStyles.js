14a6618333d1d06c559c44b35edce7bb22f36ab2
ryankayne <coope1rk@gmail.com>
2020-01-08 14:57:34 -0500

    More styles

--- foodie/src/styles/NavStyles.js
+++ foodie/src/styles/NavStyles.js
@@ -5,12 +5,10 @@
 
 import styled from "styled-components";
 
-
-
 export const Header = styled.header`
   display:flex
   background: #C45228
-  height: 20vh
+  height: 65px;
   justify-content: space-around
   align-items:center
   h1{
@@ -19,7 +17,7 @@ export const Header = styled.header`
   }
 
   @media (max-width: 500px){
-      height:10vh
+      // height:10vh
    h1{
        font-size:2rem
    }
@@ -30,8 +28,8 @@ export const DropDown = styled.ul`
   flex-direction: column;
   margin: 0;
   padding: 15px;
-  border-left: solid black 2px;
-  border-bottom: solid black 2px;
+  // border-left: solid black 2px;
+  // border-bottom: solid black 2px;
 
   a{
     text-decoration:none;
